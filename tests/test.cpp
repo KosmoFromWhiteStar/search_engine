12f3691fed22e873f2c2c410f33e8abe6766aa68
Kosmo <The_Kosmo@outlook.com>
2023-09-13 22:34:27 +0500

    separated test and main prog.

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,7 +5,7 @@
 #include "gTest_Searc.h"
 
 
-int main() {
-    ::testing::InitGoogleTest();
+int main(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc,argv);
     return RUN_ALL_TESTS();
 }
